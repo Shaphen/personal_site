09ad4817d29326548d42cf356579f2e302dbb73a
Shaphen <swpburn@gmail.com>
2020-06-09 22:01:05 -0700

    Add animations to project gifs

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -10,4 +10,27 @@ $(window).scroll(function () {
       $(this).removeClass('slide-right');
     }
   });
+
+  $('#second-project').each(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(this).position().top
+      && $(window).scrollTop() < $(this).position().top + $(this).height()) {
+      if (!$(this).hasClass('slide-left')) {
+        $(this).addClass('slide-left');
+      }
+    }
+    else if ($(this).hasClass('slide-left')) {
+      $(this).removeClass('slide-left');
+    }
+  });
+
+  $('#third-project').each(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(this).position().top // if window bottom position is greater than ele top positon
+      && $(window).scrollTop() < $(this).position().top + $(this).height()) { // if window top position is less than ele bottom positon
+      if (!$(this).hasClass('slide-right')) {
+        $(this).addClass('slide-right');
+      }
+    } else {
+      $(this).removeClass('slide-right');
+    }
+  });
 });
