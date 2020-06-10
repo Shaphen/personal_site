27e14e7a99854e4ef92e4c8314df3f20420d4acf
Shaphen <swpburn@gmail.com>
2020-06-09 22:29:22 -0700

    Add aniamtion to skills list and project text

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -11,7 +11,7 @@ $(window).scroll(function () {
     }
   });
 
-  $('#second-project').each(function () {
+  $('.first-project-text').each(function () {
     if ($(window).scrollTop() + $(window).height() >= $(this).position().top
       && $(window).scrollTop() < $(this).position().top + $(this).height()) {
       if (!$(this).hasClass('slide-left')) {
@@ -20,6 +20,30 @@ $(window).scroll(function () {
     }
     else if ($(this).hasClass('slide-left')) {
       $(this).removeClass('slide-left');
+    }
+  });
+  
+  $('#second-project').each(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(this).position().top
+    && $(window).scrollTop() < $(this).position().top + $(this).height()) {
+      if (!$(this).hasClass('slide-left')) {
+        $(this).addClass('slide-left');
+      }
+    }
+    else if ($(this).hasClass('slide-left')) {
+      $(this).removeClass('slide-left');
+    }
+  });
+
+  $('.second-project-text').each(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(this).position().top
+      && $(window).scrollTop() < $(this).position().top + $(this).height()) {
+      if (!$(this).hasClass('slide-right')) {
+        $(this).addClass('slide-right');
+      }
+    }
+    else if ($(this).hasClass('slide-right')) {
+      $(this).removeClass('slide-right');
     }
   });
 
@@ -31,6 +55,29 @@ $(window).scroll(function () {
       }
     } else {
       $(this).removeClass('slide-right');
+    }
+  });
+
+  $('.third-project-text').each(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(this).position().top
+      && $(window).scrollTop() < $(this).position().top + $(this).height()) {
+      if (!$(this).hasClass('slide-left')) {
+        $(this).addClass('slide-left');
+      }
+    }
+    else if ($(this).hasClass('slide-left')) {
+      $(this).removeClass('slide-left');
+    }
+  });
+
+  $('#tech-list').each(function () {
+    if ($(window).scrollTop() + $(window).height() >= $(this).position().top // if window bottom position is greater than ele top positon
+      && $(window).scrollTop() < $(this).position().top + $(this).height()) { // if window top position is less than ele bottom positon
+      if (!$(this).hasClass('fade-in')) {
+        $(this).addClass('fade-in');
+      }
+    } else {
+      $(this).removeClass('fade-in');
     }
   });
 });
